a71977727dac65bfcd2a85ba040b6a6a0aea2e0e
M.Emanuel <M.Emanuel@NB-0342>
2025-06-09 19:04:29 +0200

    Fehlerbehebung für App-Klasse und UI-Verbesserungen

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,15 +1,17 @@
-﻿namespace PixFrameWorkspace
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
-        }
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+namespace PixFrameWorkspace;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new MainPage();
     }
+
+    // WICHTIG: Diese Methode muss vorhanden sein
+    public static override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
